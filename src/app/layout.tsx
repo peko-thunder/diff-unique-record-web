6b89388d60ca2d53cb9906cb234d923abfa8cae0
peko-thunder <eag000gle.0.3151993@gmail.com>
2024-02-29 22:29:08 +0900

    fix: デザイン修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <body className={`grid grid-rows-[auto_1fr] min-h-lvh ${inter.className}`}>
-        <header className="bg-white/70 px-8 py-4">
+        <header className="flex justify-center bg-white/70 px-8 py-4">
           <div className="flex items-center max-w-5xl w-full">
             <h1 className="font-bold italic">Record Diff Checker</h1>
           </div>
         </header>
-        <main className="p-8">
+        <main className="flex justify-center p-8">
           <div className="flex max-w-5xl w-full h-full">{children}</div>
         </main>
       </body>
